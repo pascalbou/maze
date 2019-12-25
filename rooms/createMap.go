87571566f250eed9f9494a285812deb2bb6ad656
krikaz <pascal.bou@protonmail.com>
2019-12-25 17:22:09 +0100

    used specific int

--- rooms/createMap.go
+++ rooms/createMap.go
@@ -6,28 +6,29 @@ import (
 )
 
 type room struct {
-	id          int
+	id          uint16
 	title       string
 	description string
-	// players     []string
-	// items       []string
-	exits []string
+	exitNorth   *room
+	exitEast    *room
+	exitSouth   *room
+	exitWest    *room
+	x           int8
+	y           int8
 }
 
 func CreateRooms() {
-	var rooms [3]room
+	var rooms [500]room
+	// var wholeMap [101][101]uint8
 
-	for i := 0; i < 3; i++ {
+	var i uint16
+	for i = 0; i < 3; i++ {
 		rooms[i] = room{
 			id:          i,
 			title:       "title",
 			description: "description",
 		}
 	}
-
-	rooms[0].exits = append(rooms[0].exits, "n")
-	rooms[1].exits = append(rooms[1].exits, "n", "s")
-	rooms[2].exits = append(rooms[2].exits, "s")
 
 	connStr := "user=postgres password=test1234 dbname=maze"
 	db, err := sql.Open("postgres", connStr)
@@ -36,7 +37,9 @@ func CreateRooms() {
 	}
 	defer db.Close()
 
-	sqlStatement0 := `
+	var sqlStatement [16]string
+
+	sqlStatement[0] = `
 	drop table if exists room;
 	create table room (
 		id SERIAL,
@@ -49,7 +52,7 @@ func CreateRooms() {
 	);
 	`
 
-	sqlStatement1 := `
+	sqlStatement[1] = `
 	insert into room (title, description, exit_north, exit_south)
 	values
 		($1, $2, true, false),
@@ -57,15 +60,13 @@ func CreateRooms() {
 		($1, $2, false, true)
 	`
 
-	_, err1 := db.Exec(sqlStatement0)
+	_, err1 := db.Exec(sqlStatement[0])
 	if err1 != nil {
 		log.Fatal(err1)
 	}
 
-	_, err2 := db.Exec(sqlStatement1, "title", "description")
+	_, err2 := db.Exec(sqlStatement[1], "title", "description")
 	if err2 != nil {
 		log.Fatal(err2)
 	}
 }
-
-
