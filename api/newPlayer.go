a0e1f4d66d543fef0664b403cb8bbd127b8198f7
krikaz <pascal.bou@protonmail.com>
2019-12-27 17:55:58 +0100

    main, newplayer

--- api/newPlayer.go
+++ api/newPlayer.go
@@ -2,26 +2,53 @@ package api
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
+
+	"github.com/pascalbou/maze/lib"
 )
 
-func NewPlayer(name, token string) {
+func NewPlayerHandler(w http.ResponseWriter, r *http.Request) {
+
+	type body struct {
+		Name  string
+		Token string
+	}
+
+	var req body
+	req.Token = lib.TokenGenerator()
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	connStr := "user=postgres password=test1234 dbname=maze"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
-	q := `
+	
+	sqlStatement := `
 	insert into account (name, token, current_room)
 	values
-		($1, $2, 1)
+	($1, $2, 1)
 	`
-
-	_, err1 := db.Exec(q, name, token)
-	if err1 != nil {
-		panic(err1)
+	
+	_, errQ := db.Exec(sqlStatement, req.Name, req.Token)
+	if errQ != nil {
+		log.Fatal(errQ)
+	}
+	
+	response, err := json.Marshal(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
--- main.go
+++ main.go
@@ -1,58 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 	"github.com/pascalbou/maze/api"
-	"github.com/pascalbou/maze/lib"
 	"github.com/pascalbou/maze/rooms"
 )
-
-type body struct {
-	Name  string
-	Token string
-}
-
-func newPlayerHandler(w http.ResponseWriter, r *http.Request) {
-	token := lib.TokenGenerator()
-	// fmt.Println(len(token))
-
-	var b body
-	decoder := json.NewDecoder(r.Body)
-	errD := decoder.Decode(&b)
-	if errD != nil {
-		panic(errD)
-	}
-
-	api.NewPlayer(b.Name, token)
-
-	newBody := body{Name: b.Name, Token: token}
-	response, err := json.Marshal(newBody)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(200)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(response)
-}
-
 
 func main() {
 	// api.CreateDB()
 	rooms.CreateRooms()
 
-	http.HandleFunc("/newplayer", newPlayerHandler)
+	http.HandleFunc("/newplayer", api.NewPlayerHandler)
 	http.HandleFunc("/init", api.InitHandler)
 	http.HandleFunc("/move", api.MoveHandler)
 
 	log.Println("Listening on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
-
-
 
 }
